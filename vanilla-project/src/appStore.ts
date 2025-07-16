b07e03636b855e72d6760dcb64334e6aa4206255
Romi Sagi <romis@bynet.co.il>
2025-07-16 16:02:07 +0300

    fix changes

---
+++ vanilla-project/src/appStore.ts
@@ -0,0 +1,35 @@
+import mock from "./mocks";
+import './style/card.css';
+import './style/img.css';
+
+interface Application {
+  id: number;
+  name: string;
+  icon: string;
+  description: string;
+  keyFeatures: string[];
+  screenshots: string[];
+}
+
+const applications: Application[] = mock;
+
+applications.forEach(app => {
+  const appsContainer = document.getElementById("card-container");
+
+  if (appsContainer) {
+    const appCard: HTMLDivElement = document.createElement('div');
+    appCard.className = 'app-card';
+
+    const appIcon: HTMLImageElement = document.createElement('img');
+    appIcon.className = 'image';
+    appIcon.src = app.icon;
+
+    const appName: HTMLDivElement = document.createElement('div');
+    appName.textContent = app.name;
+
+    appCard.appendChild(appIcon);
+    appCard.appendChild(appName);
+
+    appsContainer.appendChild(appCard);
+  }
+})
